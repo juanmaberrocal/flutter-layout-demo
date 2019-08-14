223602cf02195cd4a6c42896cf74fbb286982d2f
juanmaberrocal <juanmaberrocal37@gmail.com>
2019-08-14 09:43:23 -0400

    Step 3: Implement the button row

--- lib/main.dart
+++ lib/main.dart
@@ -57,6 +57,27 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
   Widget titleSection = Container(
     padding: const EdgeInsets.all(32),
     child: Row(
@@ -97,6 +118,20 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+
+    Color color = Theme.of(context).primaryColor;
+
+    Widget buttonSection = Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          _buildButtonColumn(color, Icons.call, 'CALL'),
+          _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+          _buildButtonColumn(color, Icons.share, 'SHARE'),
+        ],
+      ),
+    );
+
     // This method is rerun every time setState is called, for instance as done
     // by the _incrementCounter method above.
     //
@@ -111,7 +146,8 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Column(            
         children: [            
-          titleSection,            
+          titleSection,
+          buttonSection,
         ],
       ),
       floatingActionButton: FloatingActionButton(
